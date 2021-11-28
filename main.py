95ce9089a38fd12179d78cfa9ff7c1eac0b9d77f
Shaked <sakee121@gmail.com>
2021-11-28 13:46:22 +0200

    Sort column by click (another click will reverse) added scrollbar (x,y)

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ from tkinter import *
 from tkinter import ttk
 import sqlite3
 
+
 class App:
     def __init__(self):
         self.root = Tk()
-        self.root.geometry("500x500")
+        self.root.geometry("1000x500")
         self.root.title("Artist, Albums, Tracks")
 
         self.root.wm_attributes("-topmost", 1)
         self.frame = Frame(self.root)
         self.frame.pack()
         self.tree = ttk.Treeview(self.frame, columns=(1, 2, 3, 4, 5, 6, 7), \
-                            height=20, show="headings")
-        self.tree.bind("<Double-1>", self.on_double_click)
+                                 height=20, show="headings")
+
+        vsb = ttk.Scrollbar(self.frame, orient="vertical", command=self.tree.yview)
+        vsb.pack(side='right', fill='y')
+        self.tree.configure(yscrollcommand=vsb.set)
+
+        vsbX = ttk.Scrollbar(self.frame, orient="horizontal", command=self.tree.xview)
+        vsbX.pack(side='bottom', fill='x')
+        self.tree.configure(xscrollcommand=vsbX.set)
+
+        #self.tree.bind("<Double-1>", self.on_double_click)
+        self.lastClick = None
+        self.tree.bind("<Button-1>", self.on_double_click)
 
         self.tree.pack(side='top')
 
@@ -25,32 +37,38 @@ class App:
         self.tree.heading(6, text="UnitPrice")
         self.tree.heading(7, text="Composer")
 
-        # for i in range(1, 7):
-            # self.tree.column(i) #, width=150)
+        for i in range(1, 7):
+            self.tree.column(i, width=150)
 
         db(self.tree)
         self.root.mainloop()
 
     def on_double_click(self, event):
         region = self.tree.identify("region", event.x, event.y)
+        print(f'click : {region}')
+        print(self.tree.identify_column(event.x))
         if region == "heading":
-            columns = ('AlbumId', 'Title',"ArtistId", 'artists.Name','tracks.Name','UnitPrice','Composer' )
-            self.tree = ttk.Treeview(self.root, columns=columns, show='headings')
-            for col in columns:
-                self.tree.heading(col, text=col, command=lambda: \
-                    treeview_sort_column(self.tree, col, True))
+            col = self.tree.identify_column(event.x)
+            if self.lastClick is None or self.lastClick != col:
+                self.tree.heading(col, command=lambda: treeview_sort_column(self.tree, col, False))
+                self.lastClick = col
+
 
 def treeview_sort_column(tv, col, reverse):
     l = [(tv.set(k, col), k) for k in tv.get_children('')]
-    l.sort(reverse=reverse)
+    try:
+        l.sort(key=lambda t: int(t[0]), reverse=reverse)
+
+    except ValueError:
+        l.sort(reverse=reverse)
 
     # rearrange items in sorted positions
     for index, (val, k) in enumerate(l):
         tv.move(k, '', index)
 
     # reverse sort next time
-    tv.heading(col, command=lambda: \
-           treeview_sort_column(tv, col, not reverse))
+    tv.heading(col, command=lambda: treeview_sort_column(tv, col, not reverse))
+
 
 def db(tree):
     conn = sqlite3.connect('chinook.db')
@@ -59,40 +77,13 @@ def db(tree):
 
 
 def data(conn, mycursor, tree):
-
     for x in tree.get_children():
         tree.delete(x)
-    mycursor.execute("SELECT *  FROM artists, albums, tracks WHERE artists.ArtistId = albums.ArtistId AND albums.AlbumId = tracks.AlbumId")
+    mycursor.execute(
+        "SELECT *  FROM artists, albums, tracks WHERE artists.ArtistId = albums.ArtistId AND albums.AlbumId = tracks.AlbumId")
     for row in mycursor:
-         tree.insert('', 'end', values=row[0:10])
+        tree.insert('', 'end', values=row[0:10])
     conn.close()
 
-# def main():
-   # root.wm_attributes("-topmost", 1)
-   # frame = Frame(root)
-   # frame.pack()
-   # tree = ttk.Treeview(frame, columns = (1,2,3,4,5,6,7), \
-   #                  height = 20, show = "headings")
-   # tree.bind("<Double-1>", on_double_click)
-   #
-   # tree.pack(side = 'top')
-   #
-   # tree.heading(1, text="AlbumId")
-   # tree.heading(2, text="Title")
-   # tree.heading(3, text="ArtistId")
-   # tree.heading(4, text="artists.Name")
-   # tree.heading(5, text="tracks.Name")
-   # tree.heading(6, text="UnitPrice")
-   # tree.heading(7, text="Composer")
-   #
-   # for i in range(1, 7):
-   #     tree.column(i, width=150)
-   # # tree.column(1, width = 120)
-   # # tree.column(2, width = 120)
-   # # tree.column(3, width = 120)
-   #
-   # db(tree)
-   # root.mainloop()
 
-# main()
 app = App()
